5b751560ea87ba7bdc948adda63f6d4923f24507
Rolando <rolando.pedro.gabriel@gmail.com>
2022-03-07 17:57:26 -0600

    Mi primer commit

---
+++ src/main/java/unsis/edu/mx/mavenproject/MavenprojectApplication.java
@@ -0,0 +1,13 @@
+package unsis.edu.mx.mavenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unsis/edu/mx/mavenproject/MavenprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package unsis.edu.mx.mavenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
